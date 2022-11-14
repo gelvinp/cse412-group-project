fcbaec4d594df87d6eab2ee910a287d3011b6252
Patrick Gelvin <patrick@gelvin.dev>
2022-11-13 23:30:41 -0700

    Shaders

--- assets/scripts/main_menu/Earth.gd
+++ assets/scripts/main_menu/Earth.gd
@@ -3,13 +3,30 @@ extends CSGSphere
 export(float, 0.0, 0.5, 0.01) var rotation_speed
 var _rotation_speed = 0.0
 
-var data_texture: ImageTexture
+var data_texture = ImageTexture.new()
 
 
 func _ready():
-	#data_texture = ImageTexture.create(2700, 1350)
 	var tween = create_tween()
 	tween.tween_property(self, "_rotation_speed", rotation_speed, 1.0)
+#	var image = Image.new()
+#	image.create(8640, 4320, false, Image.FORMAT_L8)
+#	image.fill(0)
+	
+#	image.lock()
+#	for i in 8640:
+#		var value = float(i % 864) / 863
+#
+#		for j in 4320:
+#			image.set_pixel(i, j, Color(value, value, value))
+#	image.unlock()
+##
+#	data_texture.create_from_image(image, ImageTexture.FLAGS_DEFAULT)
+#
+#	(material as SpatialMaterial).albedo_texture = data_texture
+#	(material.next_pass as ShaderMaterial).set_shader_param("passthrough", false)
+#	(material.next_pass as ShaderMaterial).set_shader_param("high", Color(0.0, 1.0, 0.0))
+	
 
 
 func _process(delta):
